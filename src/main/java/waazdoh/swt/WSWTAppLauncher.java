8d3bbcef63db1a8f2b2febe45d4582313f8fea18
Juuso Vilmunen <juuso@vilmunen.net>
2014-06-18 23:02:44 +0300

    removing printStackTrace calls.

--- src/main/java/waazdoh/swt/WSWTAppLauncher.java
+++ src/main/java/waazdoh/swt/WSWTAppLauncher.java
@@ -43,7 +43,7 @@ public class WSWTAppLauncher {
 			l = ProxySelector.getDefault()
 					.select(new URI("http://waazdoh.com"));
 		} catch (URISyntaxException e) {
-			e.printStackTrace();
+			log.error(e);
 		}
 		if (l != null) {
 			for (Iterator iter = l.iterator(); iter.hasNext();) {
